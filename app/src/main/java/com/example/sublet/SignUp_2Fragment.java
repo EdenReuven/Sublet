ce229a1c0e6759074f590f6b2fee708970f08cb4
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2022-01-11 19:17:04 +0200

    sign up 2 fragment .

--- app/src/main/java/com/example/sublet/SignUp_2Fragment.java
+++ app/src/main/java/com/example/sublet/SignUp_2Fragment.java
@@ -3,7 +3,6 @@ package com.example.sublet;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,28 +11,25 @@ import android.widget.Button;
 import android.widget.EditText;
 
 
-public class SignUp_1Fragment extends Fragment {
+public class SignUp_2Fragment extends Fragment {
 
-    EditText fullName_et, userName_et, email_et, phone_et;
+    EditText password_ed, confirmPassword_ed;
     Button continue_btn;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sing_up_1, container, false);
-
-        fullName_et=view.findViewById(R.id.signUp_freg_fullName_et);
-        userName_et=view.findViewById(R.id.signUp_freg_userName_et);
-        email_et=view.findViewById(R.id.signUp_freg_email_et);
-        phone_et=view.findViewById(R.id.signUp_freg_phone_et);
+        View view = inflater.inflate(R.layout.fragment_sign_up_2, container, false);
+        password_ed =view.findViewById(R.id.signUp2_freg_password_et);
+        confirmPassword_ed=view.findViewById(R.id.signUp2_freg_conPassword_et);
         continue_btn=view.findViewById(R.id.signUp2_freg_continue_btn);
 
         continue_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(v).navigate(SignUp_1FragmentDirections.actionSignUp1FragmentToSignUp2Fragment());
+                //TODO
             }
         });
+
         return view;
     }
 }
