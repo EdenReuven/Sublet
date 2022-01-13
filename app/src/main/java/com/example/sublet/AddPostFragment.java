9864d499d04ae4f0e60a9ddc3649f0b989f245be
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2022-01-13 18:00:25 +0200

    add post1 frag.

--- app/src/main/java/com/example/sublet/AddPostFragment.java
+++ app/src/main/java/com/example/sublet/AddPostFragment.java
@@ -7,58 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddPostFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AddPostFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddPostFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddPostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddPostFragment newInstance(String param1, String param2) {
-        AddPostFragment fragment = new AddPostFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+   EditText  dateFrom_et, dateTo_et, location_et, roommate_et, price_et, people_et, bathroom_et, bedroom_et ;
+   Button continue_btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_post, container, false);
+
+        dateFrom_et=view.findViewById(R.id.addPost_freg_dateFrom_et);
+        dateTo_et=view.findViewById(R.id.addPost_freg_dateTo_et);
+        location_et=view.findViewById(R.id.addPost_freg_location_et);
+        roommate_et=view.findViewById(R.id.addPost_freg_rommaite_et);
+        price_et=view.findViewById(R.id.addPost_freg_price_et);
+        people_et=view.findViewById(R.id.addPost_freg_people_et);
+        bathroom_et=view.findViewById(R.id.addPost_freg_bathroom_et);
+        bedroom_et=view.findViewById(R.id.addPost_freg_bedroom_et);
+        continue_btn=view.findViewById(R.id.addPost_freg_continue_btn);
+
+        continue_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: move to next frag
+            }
+        });
+
+        return view;
     }
 }
