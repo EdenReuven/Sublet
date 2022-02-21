50abc588b874b8128eaa50802ba46ef9888fad07
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2022-02-22 00:00:13 +0200

    Edit photo and user details - need to fix override the previous user

--- app/src/main/java/com/example/sublet/EditProfileFragment.java
+++ app/src/main/java/com/example/sublet/EditProfileFragment.java
@@ -1,9 +1,17 @@
 package com.example.sublet;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +19,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.sublet.model.Model;
 import com.example.sublet.model.User;
 import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
 
 
 public class EditProfileFragment extends Fragment {
@@ -35,25 +46,6 @@ public class EditProfileFragment extends Fragment {
         save_btn=view.findViewById(R.id.editProfile_freg_save_btn);
         phone_et=view.findViewById(R.id.editProfile_freg_phone_et);
 
-        /*Model.instance.getUser(Model.instance.getCurrentPostId(),user -> {
-            name_et.setText(user.getUserName());
-            phone_et.setText(user.getPhone());
-            profileImg.setImageResource(R.drawable.woman);
-            if(user.getProfileUrl()!=null) {
-                Picasso.get()
-                        .load(user.getProfileUrl())
-                        .into(profileImg);
-            }
-            save_btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    user.setUserName(name_et.getText().toString());
-                    user.setPhone(phone_et.getText().toString());
-
-                }
-            });
-        });*/
-
         User user = Model.instance.getCurrentUser();
         userName_et.setText(user.getUserName());
         fullName_et.setText(user.getFullName());
@@ -64,7 +56,83 @@ public class EditProfileFragment extends Fragment {
                     .load(user.getProfileUrl())
                     .into(profileImg);
         }
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editProfileImageBitmap!=null){
+                    Model.instance.saveProfileImage(editProfileImageBitmap,userName_et.getText().toString() + ".jpg",url -> {
+                        Model.instance.getUser(Model.instance.getCurrentUser().getUserName(),user1 -> {
+                            user1.setUserName(userName_et.getText().toString());
+                            user1.setFullName(fullName_et.getText().toString());
+                            user1.setPhone(phone_et.getText().toString());
+                            user1.setProfileUrl(url);
+                            /*Model.instance.addUser(user1 ,()->{
+                                Model.instance.setCurrentUser(user1);
+                                Navigation.findNavController(v).navigate(EditProfileFragmentDirections.actionEditProfileFragmentToHomePageFragment());
+                            });*/
+                            /*Model.instance.addPost(post1,() -> {
+                                Navigation.findNavController(description_et).navigate(EditPost2FragmentDirections.actionEditPost2FragmentToHomePageFragment());
+                            });*/
+                        });
+                    });
+                }else{
+                    Model.instance.getUser(Model.instance.getCurrentUser().getUserName(),user1 -> {
+                        user1.setUserName(userName_et.getText().toString());
+                        user1.setFullName(fullName_et.getText().toString());
+                        user1.setPhone(phone_et.getText().toString());
+                    });
+                }
+            }
+
+        });
+        camera_imgBtn.setOnClickListener(v -> {
+            openCamera();
+        });
+
+        gallery_imgBtn.setOnClickListener(v -> {
+            openGallery();
+        });
 
         return view;
+    }
+
+    static final int EDIT_PROFILE_IMAGE_CAPTURE = 1;
+    static final int  EDIT_PROFILE_IMAGE_PIC =2;
+    Bitmap editProfileImageBitmap;
+
+    private void openGallery() {
+        Intent pickPictureIntent = new Intent(Intent.ACTION_PICK);
+        pickPictureIntent.setType("image/+");
+        startActivityForResult(pickPictureIntent,EDIT_PROFILE_IMAGE_PIC);
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, EDIT_PROFILE_IMAGE_CAPTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDIT_PROFILE_IMAGE_CAPTURE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Bundle extras = data.getExtras();
+                editProfileImageBitmap = (Bitmap) extras.get("data");
+                profileImg.setImageBitmap(editProfileImageBitmap);
+
+            }
+        } else if (requestCode == EDIT_PROFILE_IMAGE_PIC) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    final Uri imgUri = data.getData();
+                    final InputStream imgStream = getContext().getContentResolver().openInputStream(imgUri);
+                    editProfileImageBitmap = BitmapFactory.decodeStream(imgStream);
+                    profileImg.setImageBitmap(editProfileImageBitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "Failed to select image", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
